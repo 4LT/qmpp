e9f0e2d85a74d83d96ed2d9ff0090eaf0ae509ee
seth <rader.seth@gmail.com>
2021-10-17 20:52:07 -0400

    cargo fmt

--- qmpp-hello-plugin/src/lib.rs
+++ qmpp-hello-plugin/src/lib.rs
@@ -72,48 +72,49 @@ pub extern "C" fn QMPP_Hook_process() {
 
     let entity_ct = unsafe { QMPP_ehandle_count() };
 
-    let (brush_ct, surface_ct) = (0..entity_ct).map(|ehandle| {
-        let mut ent_brush_ct = MaybeUninit::<u32>::uninit();
+    let (brush_ct, surface_ct) = (0..entity_ct)
+        .map(|ehandle| {
+            let mut ent_brush_ct = MaybeUninit::<u32>::uninit();
 
-        let status = unsafe {
-            QMPP_bhandle_count( ehandle, ent_brush_ct.as_mut_ptr())
-        };
+            let status = unsafe {
+                QMPP_bhandle_count(ehandle, ent_brush_ct.as_mut_ptr())
+            };
 
-        if status == qmpp_shared::SUCCESS {
-            let ent_brush_ct = unsafe { ent_brush_ct.assume_init() };
+            if status == qmpp_shared::SUCCESS {
+                let ent_brush_ct = unsafe { ent_brush_ct.assume_init() };
 
-            let ent_surface_ct = (0..ent_brush_ct).map(|bhandle| {
-                let mut brush_surface_ct = MaybeUninit::<u32>::uninit();
+                let ent_surface_ct = (0..ent_brush_ct)
+                    .map(|bhandle| {
+                        let mut brush_surface_ct = MaybeUninit::<u32>::uninit();
 
-                let status = unsafe {
-                    QMPP_shandle_count(
-                        ehandle,
-                        bhandle,
-                        brush_surface_ct.as_mut_ptr(),
-                    )
-                };
+                        let status = unsafe {
+                            QMPP_shandle_count(
+                                ehandle,
+                                bhandle,
+                                brush_surface_ct.as_mut_ptr(),
+                            )
+                        };
 
-                if status == qmpp_shared::SUCCESS {
-                    unsafe { brush_surface_ct.assume_init() }
-                } else {
-                    0
-                }
-            }).sum();
+                        if status == qmpp_shared::SUCCESS {
+                            unsafe { brush_surface_ct.assume_init() }
+                        } else {
+                            0
+                        }
+                    })
+                    .sum();
 
-            (ent_brush_ct, ent_surface_ct)
-        } else {
-            (0, 0)
-        }
-    }).fold(
-        (0, 0),
-        |(b_accum, s_accum), (b_ct, s_ct)| (b_accum+b_ct, s_accum+s_ct)
-    );
+                (ent_brush_ct, ent_surface_ct)
+            } else {
+                (0, 0)
+            }
+        })
+        .fold((0, 0), |(b_accum, s_accum), (b_ct, s_ct)| {
+            (b_accum + b_ct, s_accum + s_ct)
+        });
 
     let mesg = format!(
         "Found {} surfaces in {} brushes in {} entities",
-        surface_ct,
-        brush_ct,
-        entity_ct,
+        surface_ct, brush_ct, entity_ct,
     );
 
     unsafe {
@@ -137,8 +138,11 @@ extern "C" {
 
     pub fn QMPP_bhandle_count(ehandle: u32, brush_ct_ptr: *mut u32) -> u32;
 
-    pub fn QMPP_shandle_count(ehandle: u32, bhandle: u32,
-                              surface_ct_ptr: *mut u32) -> u32;
+    pub fn QMPP_shandle_count(
+        ehandle: u32,
+        bhandle: u32,
+        surface_ct_ptr: *mut u32,
+    ) -> u32;
 }
 
 #[panic_handler]
